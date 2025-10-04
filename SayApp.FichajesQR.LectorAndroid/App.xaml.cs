8e5888969ae66da61c18775e2e0794f970498893
Mikel IO - @tximikel <tximikel@gmail.com>
2025-10-04 10:50:45 +0200

    Nuevo Proyecto Lector para Android con Aplicación .NET MAUI, funcional en emulador Android 13 con página de Hello World

---
+++ SayApp.FichajesQR.LectorAndroid/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace SayApp.FichajesQR.LectorAndroid
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
